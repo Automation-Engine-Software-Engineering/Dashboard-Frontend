2a9657742409016e2356d8a18de581f1c99673d1
Sepehr Shirazi <sepehrshirazi27@gmail.com>
2025-03-15 21:15:48 +0330

    rename: fix `entity` file name

--- src/hooks/useFormEntities.ts
+++
@@ -1,8 +0,0 @@
-import { getAllEntities } from "@/api/entitiy";
-import { useQuery } from "@tanstack/react-query";
-
-export const useFormEntities = () =>
-  useQuery({
-    queryFn: getAllEntities,
-    queryKey: ["entities"]
-  });
