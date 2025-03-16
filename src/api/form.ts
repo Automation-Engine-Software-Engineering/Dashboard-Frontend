729042fc24dfcdd76957ed5b2a38e4c3e1d2474b
Sepehr Shirazi <sepehrshirazi27@gmail.com>
2025-03-16 07:56:20 +0330

    feat: add `create` and `edit` form api call

--- src/api/form.ts
+++ src/api/form.ts
@@ -1,5 +1,6 @@
 import { apiResponseMiddleware } from "@/middleware/api-response";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 import { FormType } from "@/types/form/form";
 
@@ -24,6 +25,34 @@ export const getForm = async (
     () => {},
     {
       showToast: false
+    }
+  );
+};
+
+export const createForm = async (form: Partial<FormType>) => {
+  return await apiResponseMiddleware<FormType>(
+    axios.post(`${API_URL}/${API_ENDPOINT}/create`, form),
+    () => {
+      toast.success("فرم با موفقیت ساخته شد", {
+        id: "api-middleware"
+      });
+    },
+    {
+      showToast: true
+    }
+  );
+};
+
+export const editForm = async (form: Partial<FormType>) => {
+  return await apiResponseMiddleware<FormType>(
+    axios.post(`${API_URL}/${API_ENDPOINT}/update`, form),
+    () => {
+      toast.success("فرم با موفقیت ویرایش شد", {
+        id: "api-middleware"
+      });
+    },
+    {
+      showToast: true
     }
   );
 };
