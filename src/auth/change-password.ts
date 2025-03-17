e628adf940fbdad59fce36eaca6f9bea5ca809e6
Sepehr Shirazi <sepehrshirazi27@gmail.com>
2025-03-17 14:46:45 +0330

    feat: implement `change password` api call

---
+++ src/auth/change-password.ts
@@ -0,0 +1,34 @@
+import { axiosInstance } from "@/api/axios-instance";
+import { apiResponseMiddleware } from "@/middleware/api-response";
+
+import { SessionType } from "@/types/session";
+
+import { getToken } from "./index";
+
+const API_ENDPOINT = "Professor/UpdatePassword";
+
+export const changeSessionPassword = async ({
+  newPassword,
+  oldPassword
+}: {
+  newPassword: string;
+  oldPassword: string;
+}): Promise<SessionType | null> => {
+  const token = getToken();
+
+  return await apiResponseMiddleware<SessionType>(
+    axiosInstance.post(API_ENDPOINT, {
+      data: {
+        id: token,
+        oldPassword,
+        newPassword
+      }
+    }),
+    () => {
+      window.location.replace("/");
+    },
+    {
+      showToast: true
+    }
+  );
+};
