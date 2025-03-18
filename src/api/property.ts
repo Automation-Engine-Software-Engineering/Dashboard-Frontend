b9ecd63be635561736d65dd90a21211b4de3235a
Sepehr Shirazi <sepehrshirazi27@gmail.com>
2025-03-18 12:07:48 +0330

    feat: implement property api calls

--- src/api/property.ts
+++ src/api/property.ts
@@ -1,18 +1,62 @@
 import { apiResponseMiddleware } from "@/middleware/api-response";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 import { PropertyType } from "@/types/form/property";
 
 const API_URL = import.meta.env.VITE_FORM_API_URL as string;
+const API_ENDPOINT = "api/Property";
 
 export const getEntityProperties = async (
   entityId: string | number
 ): Promise<PropertyType[] | null> => {
   return await apiResponseMiddleware<PropertyType[]>(
-    axios.get(`${API_URL}/${entityId}/peroperty`),
+    axios.get(`${API_URL}/${API_ENDPOINT}/${entityId}`),
     () => {},
     {
       showToast: false
     }
   );
 };
+
+export const createProperty = async (property: Partial<PropertyType>) => {
+  return await apiResponseMiddleware<PropertyType>(
+    axios.post(`${API_URL}/${API_ENDPOINT}/entity/property/add`, property),
+    () => {
+      toast.success("عنصر با موفقیت ساخته شد", {
+        id: "api-middleware"
+      });
+    },
+    {
+      showToast: true
+    }
+  );
+};
+
+export const editProperty = async (propertyId: Partial<PropertyType>) => {
+  return await apiResponseMiddleware<PropertyType>(
+    axios.post(`${API_URL}/${API_ENDPOINT}/property/update`, propertyId),
+    () => {
+      toast.success("عنصر با موفقیت ویرایش شد", {
+        id: "api-middleware"
+      });
+    },
+    {
+      showToast: true
+    }
+  );
+};
+
+// export const deleteProperty = async (propertyId: number) => {
+//   return await apiResponseMiddleware<PropertyType>(
+//     axios.post(`${API_URL}/${API_ENDPOINT}/remove`, propertyId),
+//     () => {
+//       toast.success("عنصر با موفقیت حذف شد", {
+//         id: "api-middleware"
+//       });
+//     },
+//     {
+//       showToast: true
+//     }
+//   );
+// };
