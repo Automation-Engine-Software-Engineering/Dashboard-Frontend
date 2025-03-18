f7f7258392450d2b43449c2a3bf538ccf36eb792
Sepehr Shirazi <sepehrshirazi27@gmail.com>
2025-03-18 06:11:57 +0330

    feat: implement `entity` api calls

--- src/api/entity.ts
+++ src/api/entity.ts
@@ -1,17 +1,60 @@
 import { apiResponseMiddleware } from "@/middleware/api-response";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 import { EntityType } from "@/types/form/entity";
 
 const API_URL = import.meta.env.VITE_FORM_API_URL as string;
-const API_ENDPOINT = "api/Entity/all";
+const API_ENDPOINT = "api/Entity";
 
 export const getAllEntities = async (): Promise<EntityType[] | null> => {
   return await apiResponseMiddleware<EntityType[]>(
-    axios.get(`${API_URL}/${API_ENDPOINT}`),
+    axios.get(`${API_URL}/${API_ENDPOINT}/all`),
     () => {},
     {
       showToast: false
+    }
+  );
+};
+
+export const createEntity = async (entity: Partial<EntityType>) => {
+  return await apiResponseMiddleware<EntityType>(
+    axios.post(`${API_URL}/${API_ENDPOINT}/create`, entity),
+    () => {
+      toast.success("جدول با موفقیت ساخته شد", {
+        id: "api-middleware"
+      });
+    },
+    {
+      showToast: true
+    }
+  );
+};
+
+export const editEntity = async (entity: Partial<EntityType>) => {
+  return await apiResponseMiddleware<EntityType>(
+    axios.post(`${API_URL}/${API_ENDPOINT}/update`, entity),
+    () => {
+      toast.success("جدول با موفقیت ویرایش شد", {
+        id: "api-middleware"
+      });
+    },
+    {
+      showToast: true
+    }
+  );
+};
+
+export const deleteEntity = async (entityId: number) => {
+  return await apiResponseMiddleware<EntityType>(
+    axios.post(`${API_URL}/${API_ENDPOINT}/remove`, entityId),
+    () => {
+      toast.success("فرم با موفقیت حذف شد", {
+        id: "api-middleware"
+      });
+    },
+    {
+      showToast: true
     }
   );
 };
