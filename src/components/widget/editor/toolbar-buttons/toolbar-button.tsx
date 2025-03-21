ec047ea55e4ea569fb3a3ffeffc17856ece7ffce
Sepehr Shirazi <sepehrshirazi27@gmail.com>
2025-03-21 10:06:49 +0330

    chore: refactor `ToolbarButton` to support `forwardRef`

--- src/components/widget/editor/toolbar-buttons/toolbar-button.tsx
+++ src/components/widget/editor/toolbar-buttons/toolbar-button.tsx
@@ -1,19 +1,22 @@
+import { forwardRef } from "react";
+
 import { cn } from "@/lib/utils";
 
 const ToolbarButton: React.FC<
   React.PropsWithChildren<React.ComponentProps<"button">>
-> = ({ children, className, ...props }) => {
+> = forwardRef(({ children, className, ...props }, ref) => {
   return (
     <button
       className={cn(
         "flex size-8 items-center justify-center rounded-md p-1 text-slate-900 transition-colors hover:bg-primary/10",
         className
       )}
+      ref={ref}
       {...props}
     >
       {children}
     </button>
   );
-};
+});
 
 export default ToolbarButton;
