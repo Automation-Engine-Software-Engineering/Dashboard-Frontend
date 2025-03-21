1948eea6eab4ce099e0bec4bff95e01a6576fbdb
Sepehr Shirazi <sepehrshirazi27@gmail.com>
2025-03-21 18:25:00 +0330

    feat: add ordered and unordered list toolbar buttons

---
+++ src/components/widget/editor/toolbar-buttons/list-order.tsx
@@ -0,0 +1,65 @@
+import { ListOrderedIcon, ListIcon } from "lucide-react";
+import { useEffect, useState } from "react";
+
+import { checkState } from "@/helpers/editor/check-state";
+
+import { cn } from "@/lib/utils";
+
+import ToolbarButton from "./toolbar-button";
+
+const ListOrder: React.FC<
+  React.PropsWithChildren<
+    React.ComponentProps<"button"> & {
+      editorRef: React.RefObject<HTMLDivElement>;
+    }
+  >
+> = ({ className, editorRef, ...props }) => {
+  const [isOrderedList, setIsOrderedList] = useState<boolean>(false);
+  const [isUnorderedList, setIsUnorderedList] = useState<boolean>(false);
+
+  const handleCommand = (command: string) => {
+    document.execCommand(command, false);
+    editorRef?.current?.focus();
+  };
+
+  useEffect(() => {
+    const handleSelectionChange = () => {
+      setIsOrderedList(!!checkState("insertOrderedList"));
+      setIsUnorderedList(!!checkState("insertUnorderedList"));
+    };
+
+    document.addEventListener("selectionchange", handleSelectionChange);
+
+    return () => {
+      document.removeEventListener("selectionchange", handleSelectionChange);
+    };
+  }, []);
+
+  return (
+    <>
+      <ToolbarButton
+        className={cn(
+          className,
+          isOrderedList ? "bg-primary/10" : "bg-transparent"
+        )}
+        onClick={() => handleCommand("insertOrderedList")}
+        {...props}
+      >
+        <ListOrderedIcon />
+      </ToolbarButton>
+
+      <ToolbarButton
+        className={cn(
+          className,
+          isUnorderedList ? "bg-primary/10" : "bg-transparent"
+        )}
+        onClick={() => handleCommand("insertUnorderedList")}
+        {...props}
+      >
+        <ListIcon />
+      </ToolbarButton>
+    </>
+  );
+};
+
+export default ListOrder;
