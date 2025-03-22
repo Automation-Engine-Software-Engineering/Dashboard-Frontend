a45add06a432d634e8c6be0a9967389bd67cec4e
Sepehr Shirazi <sepehrshirazi27@gmail.com>
2025-03-22 12:12:33 +0330

    hotfix build time error

--- src/components/common/highligh-input/index.tsx
+++ src/components/common/highligh-input/index.tsx
@@ -1,9 +1,4 @@
-import React, {
-  forwardRef,
-  useEffect,
-  useImperativeHandle,
-  useRef
-} from "react";
+import { forwardRef, useEffect, useImperativeHandle, useRef } from "react";
 
 interface HighlightInputProps {
   onChange?: (value: string) => void;
