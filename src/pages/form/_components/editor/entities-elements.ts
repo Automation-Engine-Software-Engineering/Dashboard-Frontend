ea8e6b00a13f1c3acab62eb72f042d3d8d7e716c
Sepehr Shirazi <sepehrshirazi27@gmail.com>
2025-03-23 22:36:28 +0330

    fix: set default width for inputs

--- src/pages/form/_components/editor/entities-elements.ts
+++ src/pages/form/_components/editor/entities-elements.ts
@@ -22,7 +22,7 @@ export const createInput = ({
 }: InputProps): string => {
   const commonStyles = "width: 100%; height: 100%; box-sizing: border-box;";
 
-  const wrapper = `<div id="input-wrapper" class="wrapper" data-type="${type}" contenteditable="false" style="display: inline-block; max-width:100%;resize: both; height:50px" oninput="event.preventDefault();">`;
+  const wrapper = `<div id="input-wrapper" class="wrapper" data-type="${type}" contenteditable="false" style="display: inline-block; max-width:100%;resize: both; height:50px; width:50px" oninput="event.preventDefault();">`;
 
   const input = `<input 
     id="${inputId}"
