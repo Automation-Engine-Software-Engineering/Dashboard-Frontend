d17e9f4a07d5984b4489449bb7c4451b3ecf3729
Sepehr Shirazi <sepehrshirazi27@gmail.com>
2025-03-25 04:15:42 +0330

    feat: enhance `InsertHorizonLine` with customizable margins

--- src/components/widget/editor/toolbar-buttons/insert-horizon-line.tsx
+++ src/components/widget/editor/toolbar-buttons/insert-horizon-line.tsx
@@ -1,6 +1,16 @@
 import { MinusIcon } from "lucide-react";
+import { useState } from "react";
 
 import { cn } from "@/lib/utils";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger
+} from "@/components/ui/tooltip";
 
 import ToolbarButton from "./toolbar-button";
 
@@ -11,29 +21,74 @@ const InsertHorizonLine: React.FC<
     }
   >
 > = ({ className, editorRef, ...props }) => {
-  const handleCommand = (command: string) => {
+  const [topMargin, setTopMargin] = useState(0);
+  const [bottomMargin, setBottomMargin] = useState(0);
+
+  const handleInsertHorizontalLine = () => {
     const selection = window.getSelection();
 
-    if (selection?.rangeCount) {
-      const range = selection.getRangeAt(0);
-
-      if (editorRef.current?.contains(range.commonAncestorContainer)) {
-        document.execCommand(command, true);
-        editorRef?.current?.focus();
+    if (editorRef.current) {
+      if (selection?.rangeCount) {
+        const range = selection.getRangeAt(0);
+        const hr = document.createElement("hr");
+        hr.style.marginTop = `${topMargin}px`;
+        hr.style.marginBottom = `${bottomMargin}px`;
+        if (editorRef.current?.contains(range.commonAncestorContainer)) {
+          range.deleteContents();
+          range.insertNode(hr);
+          range.setStartAfter(hr);
+          range.setEndAfter(hr);
+        } else {
+          editorRef.current.appendChild(hr);
+        }
       }
     }
   };
 
   return (
-    <ToolbarButton
-      className={cn(className)}
-      onClick={() => {
-        handleCommand("insertHorizontalRule");
-      }}
-      {...props}
-    >
-      <MinusIcon />
-    </ToolbarButton>
+    <TooltipProvider delayDuration={0}>
+      <Tooltip>
+        <TooltipTrigger
+          asChild
+          onClick={() => {
+            document.execCommand("insertHorizontalRule", true);
+          }}
+        >
+          <ToolbarButton className={cn(className)} {...props}>
+            <MinusIcon />
+          </ToolbarButton>
+        </TooltipTrigger>
+        <TooltipContent side="bottom" className="grid w-64 grid-cols-2 gap-2">
+          <Input
+            type="number"
+            value={topMargin}
+            defaultValue={0}
+            min={0}
+            pattern="[0-9]*"
+            onChange={(e) => {
+              setTopMargin(+e.target.value);
+            }}
+          />
+          <Input
+            type="number"
+            value={bottomMargin}
+            defaultValue={0}
+            min={0}
+            pattern="[0-9]*"
+            onChange={(e) => {
+              setBottomMargin(+e.target.value);
+            }}
+          />
+
+          <Button
+            className="col-span-full"
+            onClick={handleInsertHorizontalLine}
+          >
+            Insert
+          </Button>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 };
 
