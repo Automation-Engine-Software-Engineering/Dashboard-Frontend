2cdc535de2caf3bd34b5ce6d015feb112eab9bdc
Sepehr Shirazi <sepehrshirazi27@gmail.com>
2025-03-26 00:25:36 +0330

    feat: implement API request and response interceptors for token management

--- src/api/axios-instance.ts
+++ src/api/axios-instance.ts
@@ -1,7 +1,54 @@
+import { getToken, setToken } from "@/auth";
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL as string;
+const API_URL = import.meta.env.VITE_FORM_API_URL as string;
 
-export const axiosInstance = axios.create({
-  baseURL: API_URL
+export const apiProfile = axios.create({
+  baseURL: import.meta.env.VITE_API_URL as string
 });
+
+export const api = axios.create({ baseURL: API_URL });
+
+api.interceptors.request.use(
+  (config) => {
+    const { accessToken } = getToken();
+    if (accessToken) {
+      config.headers["Authorization"] = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 403 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      const { refreshToken } = getToken();
+
+      const response = await axios.post(
+        `${API_URL}/api/Authentication/GenerateToken`,
+        JSON.stringify(refreshToken),
+        {
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }
+      );
+      if (response.status === 200) {
+        const newToken = response.data.data;
+        setToken({ accessToken: newToken, refreshToken: refreshToken! });
+        axios.defaults.headers.common["Authorization"] = `Bearer ${newToken}`;
+        originalRequest.headers["Authorization"] = `Bearer ${newToken}`;
+        return axios(originalRequest);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
