6883c32ca2e1a0bd3f69502e6ca931cdd11416bc
Sepehr Shirazi <sepehrshirazi27@gmail.com>
2025-03-27 02:31:20 +0330

    feat: integrate role creation and editing functionality in RoleModal with loading state

--- src/components/common/modals/role-modal/index.tsx
+++ src/components/common/modals/role-modal/index.tsx
@@ -1,6 +1,7 @@
 import { memo } from "react";
 
-import toast from "react-hot-toast";
+import { createRole, editRole } from "@/api/role";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import { RoleType } from "@/types/role";
 
@@ -37,48 +38,60 @@ const RoleModal: React.FC<Props> = ({ isOpen, onClose, role }) => {
           <DialogTitle>{title}</DialogTitle>
           <DialogDescription>{description}</DialogDescription>
         </DialogHeader>
-        <Content role={role} />
+        <Content role={role} onClose={onClose} />
       </DialogContent>
     </Dialog>
   );
 };
 
-const Content = memo(({ role }: { role: RoleType | null }) => {
-  console.log("hi");
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+const Content = memo(
+  ({ role, onClose }: { role: RoleType | null; onClose: () => void }) => {
+    const queryClient = useQueryClient();
 
-    // const formData = new FormData(e.currentTarget);
-    // const data = Object.fromEntries(formData);
-    toast("بزودی وصل میشه", {
-      icon: "🚧"
+    const { mutate, isPending } = useMutation({
+      mutationFn: (data: any) => (role ? editRole(data) : createRole(data)),
+      onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey: ["roles"] });
+        onClose();
+      }
     });
-  };
 
-  return (
-    <form className="space-y-5" onSubmit={handleSubmit}>
-      <div className="space-y-1">
-        <label htmlFor="">نام نقش</label>
-        <Input
-          type="text"
-          name="name"
-          placeholder="نام نفش"
-          defaultValue={role?.name ?? ""}
-        />
-      </div>
-      <div className="space-y-1">
-        <label htmlFor="">توضیحات</label>
-        <Input
-          type="text"
-          name="description"
-          placeholder="توضیحات"
-          defaultValue={role?.description ?? ""}
-        />
-      </div>
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
 
-      <Button className="w-full">ذخیره</Button>
-    </form>
-  );
-});
+      const formData = new FormData(e.currentTarget);
+      const data: Record<any, any> = Object.fromEntries(formData);
+      data.id = role!.id;
+      mutate(data);
+    };
+
+    return (
+      <form className="space-y-5" onSubmit={handleSubmit}>
+        <div className="space-y-1">
+          <label htmlFor="">نام نقش</label>
+          <Input
+            type="text"
+            name="name"
+            placeholder="نام نفش"
+            defaultValue={role?.name ?? ""}
+          />
+        </div>
+        <div className="space-y-1">
+          <label htmlFor="">توضیحات</label>
+          <Input
+            type="text"
+            name="description"
+            placeholder="توضیحات"
+            defaultValue={role?.description ?? ""}
+          />
+        </div>
+
+        <Button className="w-full" disabled={isPending}>
+          ذخیره
+        </Button>
+      </form>
+    );
+  }
+);
 
 export default RoleModal;
