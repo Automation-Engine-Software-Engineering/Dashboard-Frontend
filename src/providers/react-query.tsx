df9ad1ab237666e5e1402d50ab09474437d0f46e
Sepehr Shirazi <sepehrshirazi27@gmail.com>
2025-03-26 05:10:16 +0330

    feat: configure React Query client to disable query retries by default

--- src/providers/react-query.tsx
+++ src/providers/react-query.tsx
@@ -1,7 +1,13 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const ReactQuery = ({ children }: { children: React.ReactNode }) => {
-  const client = new QueryClient();
+  const client = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false
+      }
+    }
+  });
 
   return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
 };
