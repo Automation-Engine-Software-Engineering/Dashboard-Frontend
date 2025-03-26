1085a88b470a76557079d4d90a4942b72828fce4
Sepehr Shirazi <sepehrshirazi27@gmail.com>
2025-03-26 21:46:39 +0330

    feat: implement workflow user API functions for retrieval, creation, and removal with success notifications

--- src/api/workflow-user.ts
+++ src/api/workflow-user.ts
@@ -1,21 +1,58 @@
-import { AxiosError } from "axios";
+import { apiResponseMiddleware } from "@/middleware/api-response";
+import toast from "react-hot-toast";
+
+import { WorkflowUserType } from "@/types/workflow-user";
 
 import { api } from "./axios-instance";
 
-const API_ENDPOINT = "/api/WorkFlowUser/create";
+const API_ENDPOINT = "/api/WorkFlowUser";
 
-export const createWorkflowUser = async (
-  userId: string,
-  workFlowId: number
-): Promise<any | AxiosError<any>> => {
-  try {
-    const response = await api.post(API_ENDPOINT, {
-      id: 0,
-      userId,
-      workFlowId
-    });
-    if (!response.data.status) throw new Error(response.data.message);
-  } catch (err) {
-    throw new Error(`create workflow user error: ${err}`);
-  }
+export const getWorkFlowUserWorkflow = async (workFlowId: number) => {
+  return await apiResponseMiddleware<WorkflowUserType>(
+    api.get(`${API_ENDPOINT}/workflow`, {
+      params: {
+        workFlowId
+      }
+    }),
+    () => {
+      toast.success("اطلاعات با موفقیت دریافت شد", {
+        id: "api-middleware"
+      });
+    },
+    {
+      showToast: true
+    }
+  );
+};
+
+export const createWorkFlowUser = async (data: { workFlowId: number }) => {
+  return await apiResponseMiddleware<WorkflowUserType>(
+    api.post(`${API_ENDPOINT}/create`, data),
+    () => {
+      toast.success("اطلاعات با موفقیت ثبت شد", {
+        id: "api-middleware"
+      });
+    },
+    {
+      showToast: true
+    }
+  );
+};
+
+export const removeWorkFlowUser = async (id: number) => {
+  return await apiResponseMiddleware<WorkflowUserType>(
+    api.post(`${API_ENDPOINT}/remove`, JSON.stringify(id), {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }),
+    () => {
+      toast.success("اطلاعات با موفقیت حذف شد", {
+        id: "api-middleware"
+      });
+    },
+    {
+      showToast: true
+    }
+  );
 };
