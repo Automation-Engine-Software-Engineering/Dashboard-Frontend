cc1796f00a1f5018443e61e45637c359802f61b9
Sepehr Shirazi <sepehrshirazi27@gmail.com>
2025-03-29 04:20:27 +0330

    feat: refactor EntitiesListModal to use Dialog component and improve entity fetching logic

--- src/components/common/modals/entities-list-modal/index.tsx
+++ src/components/common/modals/entities-list-modal/index.tsx
@@ -1,143 +1,161 @@
 import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
-import { useEffect, useState } from "react";
+import { memo, useEffect, useState } from "react";
 
-import { getAllEntities } from "@/api/entity";
-import { updateFormEntities } from "@/api/form";
+import { getAllFormEntities, updateFormEntities } from "@/api/entity";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { HashLoader } from "react-spinners";
 import { useDebouncedCallback } from "use-debounce";
 
-import { EntityType } from "@/types/form/entity";
-
-import { useEntitiesListModalStore } from "@/hooks/store/use-entities-list-modal-store";
-
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle
+} from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
-import Modal from "@/components/ui/modal";
 
-const EntitiesListModal = () => {
-  const { isOpen, onClose, form } = useEntitiesListModalStore();
-  const [page, setPage] = useState<number>(1);
-  const [size] = useState<number>(10);
-  const [search, setSearch] = useState<string>("");
-  const [selectedEntities, setSelectedEntities] = useState<number[]>([]);
+interface Props {
+  formId: number;
+  isOpen: boolean;
+  onClose: () => void;
+}
 
-  const queryClient = useQueryClient();
-
-  const { data, isLoading } = useQuery({
-    queryFn: () => getAllEntities({ size, page, search }),
-    queryKey: ["entities", { page, size, search }]
-  });
-
-  const { mutate, isPending } = useMutation({
-    mutationFn: () => {
-      return updateFormEntities(form!.id, selectedEntities);
-    },
-
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["forms", form?.id] });
+const EntitiesListModal: React.FC<Props> = ({ isOpen, onClose, formId }) => {
+  const onChange = (open: boolean) => {
+    if (!open) {
+      onClose();
     }
-  });
-
-  const totalPages = Math.ceil((data?.totalCount ?? 0) / size);
-
-  const changeHandler = (entityId: number) => {
-    setSelectedEntities((prev) =>
-      prev.includes(entityId)
-        ? prev.filter((id) => id !== entityId)
-        : [...prev, entityId]
-    );
   };
 
-  const handleSearch = useDebouncedCallback((value: string) => {
-    setSearch(value);
-  }, 1000);
-
-  useEffect(() => {
-    setSelectedEntities(() => {
-      if (Array.isArray(form?.entities)) {
-        return form.entities
-          .filter(
-            (entity): entity is EntityType =>
-              typeof entity === "object" && "id" in entity
-          )
-          .map((entity) => entity.id);
-      }
-      return [];
-    });
-  }, [isOpen]);
-
   return (
-    <Modal
-      isOpen={isOpen}
-      onClose={onClose}
-      title="انتخاب جداول"
-      description="لطفا جداولی که برای فرم خود نیاز دارید را انتخاب کنید"
-    >
-      <Input
-        placeholder="جستجو"
-        onChange={(e) => {
-          handleSearch(e.target.value);
-        }}
-        className="w-full md:w-1/2"
-      />
-      <div className="mt-5 h-64 snap-y divide-y divide-slate-300 overflow-y-auto rounded-md border border-slate-300">
-        {isLoading ? (
-          <div className="flex size-full items-center justify-center">
-            <HashLoader color="#0099A5" size={50} />
-          </div>
-        ) : (
-          data?.data.map((entity) => (
-            <div
-              key={entity.id}
-              className="flex w-full snap-start items-center gap-x-2 px-2 py-4"
-            >
-              <Checkbox
-                checked={selectedEntities.includes(entity.id)}
-                onCheckedChange={() => {
-                  changeHandler(entity.id);
-                }}
-              />
-              <p className="line-clamp-1 text-sm">{entity.previewName}</p>
-            </div>
-          ))
-        )}
-      </div>
-      <div className="mt-3 flex items-center justify-between">
-        <Button
-          variant="outline"
-          onClick={() => {
-            if (page <= totalPages) {
-              setPage((prev) => prev + 1);
-            }
-          }}
-          disabled={page >= totalPages}
-        >
-          <ChevronRightIcon className="text-primary" />
-        </Button>
-        <Button
-          variant="outline"
-          onClick={() => {
-            if (page > 1) {
-              setPage((prev) => prev - 1);
-            }
-          }}
-          disabled={page <= 1}
-        >
-          <ChevronLeftIcon className="text-primary" />
-        </Button>
-      </div>
-      <Button
-        className="mt-5 w-full"
-        onClick={() => {
-          mutate();
-        }}
-        disabled={isPending}
-      >
-        ذخیره
-      </Button>
-    </Modal>
+    <Dialog open={isOpen} onOpenChange={onChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>انتخاب جداول</DialogTitle>
+        </DialogHeader>
+        <Content formId={formId} onClose={onClose} />
+      </DialogContent>
+    </Dialog>
   );
 };
+
+const Content = memo(
+  ({ formId, onClose }: { formId: number; onClose: () => void }) => {
+    const [page, setPage] = useState<number>(1);
+    const [size] = useState<number>(10);
+    const [search, setSearch] = useState<string>("");
+    const [selectedEntities, setSelectedEntities] = useState<number[]>([]);
+
+    const queryClient = useQueryClient();
+
+    const { data, isFetching: isLoading } = useQuery({
+      queryFn: () => getAllFormEntities(formId, { size, page, search }),
+      queryKey: ["form-entities", { page, size, search }]
+    });
+
+    useEffect(() => {
+      setSelectedEntities(
+        (data?.data || [])
+          .map((val) => (val.isAccess ? val.id : null))
+          .filter((val) => val !== null)
+      );
+    }, [data]);
+
+    const { mutate, isPending } = useMutation({
+      mutationFn: () => {
+        return updateFormEntities(formId, selectedEntities);
+      },
+
+      onSuccess: () => {
+        queryClient.invalidateQueries({
+          queryKey: ["form-entities", formId]
+        });
+        onClose();
+      }
+    });
+
+    const totalPages = Math.ceil((data?.totalCount ?? 0) / size);
+
+    const changeHandler = (entityId: number) => {
+      setSelectedEntities((prev) =>
+        prev.includes(entityId)
+          ? prev.filter((id) => id !== entityId)
+          : [...prev, entityId]
+      );
+    };
+
+    const handleSearch = useDebouncedCallback((value: string) => {
+      setSearch(value);
+    }, 1000);
+    return (
+      <>
+        <Input
+          placeholder="جستجو"
+          onChange={(e) => {
+            handleSearch(e.target.value);
+          }}
+          className="w-full md:w-1/2"
+        />
+        <div className="mt-5 h-64 snap-y divide-y divide-slate-300 overflow-y-auto rounded-md border border-slate-300">
+          {isLoading ? (
+            <div className="flex size-full items-center justify-center">
+              <HashLoader color="#0099A5" size={50} />
+            </div>
+          ) : (
+            data?.data.map((workflow) => (
+              <div
+                key={workflow.id}
+                className="flex w-full snap-start items-center gap-x-2 px-2 py-4"
+              >
+                <Checkbox
+                  defaultChecked={workflow.isAccess}
+                  onCheckedChange={() => {
+                    changeHandler(workflow.id);
+                  }}
+                />
+                <p className="line-clamp-1 text-sm">{workflow.name}</p>
+              </div>
+            ))
+          )}
+        </div>
+        <div className="mt-3 flex items-center justify-between">
+          <Button
+            variant="outline"
+            onClick={() => {
+              if (page <= totalPages) {
+                setPage((prev) => prev + 1);
+              }
+            }}
+            disabled={page >= totalPages}
+          >
+            <ChevronRightIcon className="text-primary" />
+          </Button>
+          <Button
+            variant="outline"
+            onClick={() => {
+              if (page > 1) {
+                setPage((prev) => prev - 1);
+              }
+            }}
+            disabled={page <= 1}
+          >
+            <ChevronLeftIcon className="text-primary" />
+          </Button>
+        </div>
+        <Button
+          className="mt-5 w-full"
+          onClick={() => {
+            mutate();
+          }}
+          disabled={isPending}
+        >
+          ذخیره
+        </Button>
+      </>
+    );
+  }
+);
+
 export default EntitiesListModal;
