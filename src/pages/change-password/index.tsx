4d31a21d6cec5f32c602a818b0eca500476d86f1
Sepehr Shirazi <sepehrshirazi27@gmail.com>
2025-03-31 22:45:57 +0330

    style: fix height of page

--- src/pages/change-password/index.tsx
+++ src/pages/change-password/index.tsx
@@ -2,7 +2,7 @@ import ChangePassword from "@/components/widget/change-password";
 
 const ChangePasswordPage = () => {
   return (
-    <div className="flex h-full w-full items-center justify-center px-5">
+    <div className="flex h-screen w-full items-center justify-center px-5">
       <div className="w-full max-w-[450px] rounded-md bg-white p-5 shadow-md">
         <h2 className="mb-10 text-center text-xl font-bold">تغییر رمز عبور</h2>
         <ChangePassword />
