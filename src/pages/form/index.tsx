58380457115afd1eb26bd3188dd1c3f230b86a51
Sepehr Shirazi <sepehrshirazi27@gmail.com>
2025-04-04 16:54:51 +0330

    feat: remove FormNavbar component from FormPage for cleaner layout

--- src/pages/form/index.tsx
+++ src/pages/form/index.tsx
@@ -1,11 +1,8 @@
 import { Outlet } from "react-router-dom";
 
-import FormNavbar from "./_components/navbar";
-
 const FormPage = () => {
   return (
     <section>
-      <FormNavbar />
       <div className="pb-5">
         <Outlet />
       </div>
