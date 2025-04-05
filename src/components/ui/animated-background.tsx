4d47d03e8ce8df17ed885e4b887341cafbdc8d96
Sepehr Shirazi <sepehrshirazi27@gmail.com>
2025-04-05 19:00:35 +0330

    remove unnecessary imports

--- src/components/ui/animated-background.tsx
+++ src/components/ui/animated-background.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { loadSlim } from "@tsparticles/slim";
 
