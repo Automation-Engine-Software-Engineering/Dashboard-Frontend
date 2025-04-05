021d20d667e995e5087722d06b3ec015cfadda2f
Sepehr Shirazi <sepehrshirazi27@gmail.com>
2025-04-05 05:57:43 +0330

    feat: refactor avatar component to use session hook and update display placeholders

--- src/components/layout/header/avatar/avatar-content.tsx
+++ src/components/layout/header/avatar/avatar-content.tsx
@@ -1,15 +1,11 @@
 import { logout } from "@/auth";
 import { Link } from "react-router-dom";
 
-import { cn, getInitialName } from "@/lib/utils";
+import { cn } from "@/lib/utils";
 
-import { useProfile } from "@/hooks/server-state/use-profile";
-
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Avatar } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { PopoverContent } from "@/components/ui/popover";
-
-const API_URL = import.meta.env.API_URL as string;
 
 const AvatarContent = () => {
   return (
@@ -24,10 +20,10 @@ const Contents: React.FC<React.ComponentProps<"div">> = ({
   className,
   ...props
 }) => {
-  const { data: session } = useProfile();
-  const sessionTitle = session?.department.titleFa;
-  const sessionFullname = `${session?.firstNameFa} ${session?.lastNameFa}`;
-  const fallbackName = getInitialName(sessionFullname);
+  // const sessionTitle = session?.department.titleFa;
+  // const sessionFullname = `${session?.firstNameFa} ${session?.lastNameFa}`;
+
+  // const fallbackName = getInitialName(sessionFullname);
 
   return (
     <div
@@ -37,13 +33,12 @@ const Contents: React.FC<React.ComponentProps<"div">> = ({
       <div className="flex flex-col">
         <div className="mb-4 flex">
           <Avatar className="ml-2 size-12 border border-[#E4EBF3]">
-            <AvatarFallback className="text-sm">{fallbackName}</AvatarFallback>
-            <AvatarImage src={`${API_URL}/${session?.imageUrl}`} />
+            {/* <AvatarFallback className="text-sm">{fallbackName}</AvatarFallback> */}
+            {/* <AvatarImage src={`${API_URL}/${session?.imageUrl}`} /> */}
           </Avatar>
           <div>
-            <h5 className="mb-2 font-medium"> {sessionFullname}</h5>
-            <p className="text-xs">{` دانشیار رشته ${sessionTitle}`}</p>
-            <p className="text-xs">دانشگاه اصفهان</p>
+            <h5 className="mb-2 font-medium">full name</h5>
+            <p className="text-xs">description</p>
           </div>
         </div>
       </div>
