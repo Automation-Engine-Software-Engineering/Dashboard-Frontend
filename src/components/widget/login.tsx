57c1185d579c0fd05e5540d05b57d7bb66004607
Sepehr Shirazi <sepehrshirazi27@gmail.com>
2025-04-07 03:02:10 +0330

    feat: add login system

--- src/components/widget/login.tsx
+++ src/components/widget/login.tsx
@@ -1,23 +1,55 @@
+import { useState } from "react";
+
+import { getUserData, signIn } from "@/api/auth";
+import { setSession, setToken } from "@/auth";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
   const navigate = useNavigate();
 
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+
+    const formData = new FormData(e.currentTarget);
+
+    const newData: Record<string, any> = {};
+
+    formData.forEach((value, key) => {
+      newData[key] = value;
+    });
+
+    try {
+      const response = await signIn(newData as any);
+
+      setToken(response.data);
+
+      const user = await getUserData(response.data);
+
+      setSession(user);
+
+      navigate("/dashboard");
+    } catch {
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
   return (
-    <div
-      className="w-full max-w-[500px] rounded-lg bg-white/10 p-10 text-white backdrop-blur"
-      dir="rtl"
-    >
+    <div className="w-full max-w-[500px] rounded-lg bg-white/10 p-10 text-white backdrop-blur">
       <h2 className="mb-10 text-center text-xl font-bold">
         ورود به حساب کاربری
       </h2>
-      <div className="space-y-10">
+      <form onSubmit={(e) => handleSubmit(e)} className="space-y-10">
         <div className="flex flex-col gap-y-2">
           <label htmlFor="" className="">
             نام کاربری
           </label>
           <input
             type="text"
+            name="username"
             placeholder="نام کاربری را وارد کنید"
             className="h-12 rounded-md bg-black/50 px-5"
           />
@@ -28,19 +60,19 @@ const Login = () => {
           </label>
           <input
             type="password"
+            name="password"
             placeholder="رمز عبور را وارد کنید"
             className="h-12 rounded-md bg-black/50 px-5"
           />
         </div>
         <button
-          onClick={() => {
-            navigate("/dashboard");
-          }}
-          className="w-full rounded-lg bg-white px-5 py-3 font-bold text-black transition-colors hover:bg-white/80"
+          type="submit"
+          className="w-full rounded-lg bg-white px-5 py-3 font-bold text-black transition-colors hover:bg-white/80 disabled:cursor-not-allowed disabled:opacity-70"
+          disabled={isSubmitting}
         >
-          ورود به حساب کاربری
+          {isSubmitting ? "درحال ورود" : "ورود به حساب کاربری"}
         </button>
-      </div>
+      </form>
     </div>
   );
 };
