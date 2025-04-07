6386d2f20ed0bed58b497adb3b18858c0725c77c
Sepehr Shirazi <sepehrshirazi27@gmail.com>
2025-04-07 06:35:40 +0330

    feat: implement workflow api call

---
+++ src/api/workflow.ts
@@ -0,0 +1,17 @@
+import { getToken } from "@/auth";
+import { AxiosError } from "axios";
+
+import { axiosInstance } from "./axios-instance";
+
+const API_ENDPOINT = "/api/Role/GetWorkFlowsByRole";
+
+export const getWorkflowByRole = async (): Promise<any | AxiosError<any>> => {
+  const token = getToken();
+
+  const response = await axiosInstance(`${API_ENDPOINT}/${token}`);
+
+  if (response.data.status) {
+    return response.data.data;
+  }
+  return null;
+};
