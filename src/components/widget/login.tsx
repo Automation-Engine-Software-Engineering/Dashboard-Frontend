ef53701ab12c232aa865cb5c52bd0ae53af46db1
Soheil Salimi <soheilsalimidev@gmail.com>
2025-04-07 10:09:11 +0330

    اضافه کردن نام شرکت به صفحه ورود

--- src/components/widget/login.tsx
+++ src/components/widget/login.tsx
@@ -38,41 +38,51 @@ const Login = () => {
   };
 
   return (
-    <div className="w-full max-w-[500px] rounded-lg bg-white/10 p-10 text-white backdrop-blur">
-      <h2 className="mb-10 text-center text-xl font-bold">
-        ورود به حساب کاربری
-      </h2>
-      <form onSubmit={(e) => handleSubmit(e)} className="space-y-10">
-        <div className="flex flex-col gap-y-2">
-          <label htmlFor="" className="">
-            نام کاربری
-          </label>
-          <input
-            type="text"
-            name="username"
-            placeholder="نام کاربری را وارد کنید"
-            className="h-12 rounded-md bg-black/50 px-5"
-          />
-        </div>
-        <div className="flex flex-col gap-y-2">
-          <label htmlFor="" className="">
-            رمز عبور
-          </label>
-          <input
-            type="password"
-            name="password"
-            placeholder="رمز عبور را وارد کنید"
-            className="h-12 rounded-md bg-black/50 px-5"
-          />
-        </div>
-        <button
-          type="submit"
-          className="w-full rounded-lg bg-white px-5 py-3 font-bold text-black transition-colors hover:bg-white/80 disabled:cursor-not-allowed disabled:opacity-70"
-          disabled={isSubmitting}
-        >
-          {isSubmitting ? "درحال ورود" : "ورود به حساب کاربری"}
-        </button>
-      </form>
+    <div className="w-full max-w-[500px] rounded-lg bg-white/5 p-10 pb-5 text-white backdrop-blur">
+      <div className="w-full max-w-[500px] rounded-lg bg-white/10 p-10 text-white backdrop-blur">
+        <h2 className="mb-10 text-center text-xl font-bold text-zinc-300">
+          ورود به حساب کاربری
+          <span  className="mr-2 text-center text-xl font-bold text-zinc-100">
+            (سامانه هوشمند)</span>
+        </h2>
+        <form onSubmit={(e) => handleSubmit(e)} className="space-y-10">
+          <div className="flex flex-col gap-y-2">
+            <label htmlFor="" className="">
+              نام کاربری
+            </label>
+            <input
+              type="text"
+              name="username"
+              placeholder="نام کاربری را وارد کنید"
+              className="h-12 rounded-md bg-black/50 px-5"
+            />
+          </div>
+          <div className="flex flex-col gap-y-2">
+            <label htmlFor="" className="">
+              رمز عبور
+            </label>
+            <input
+              type="password"
+              name="password"
+              placeholder="رمز عبور را وارد کنید"
+              className="h-12 rounded-md bg-black/50 px-5"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full rounded-lg bg-white px-5 py-3 font-bold text-black transition-colors hover:bg-white/80 disabled:cursor-not-allowed disabled:opacity-70"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "درحال ورود" : "ورود به حساب کاربری"}
+          </button>
+        </form>
+      </div>
+      <div  className="mt-5 mr-2 text-zinc-300">
+        <label>
+       +   دانشگاه اصفهان - طراحی شده توسط شرکت 
+          <span className="text-zinc-100 mr-2">پارسه آذین مبین</span>
+        </label>
+      </div>
     </div>
   );
 };
