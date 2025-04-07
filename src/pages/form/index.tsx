da9dc412abdbd6cff9c7a50fded772e0d992a190
Sepehr Shirazi <sepehrshirazi27@gmail.com>
2025-04-07 18:27:22 +0330

    feat: implement form page

---
+++ src/pages/form/index.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+
+import { getForm } from "@/api/form";
+import toast from "react-hot-toast";
+import { useNavigate, useParams } from "react-router-dom";
+import { MoonLoader } from "react-spinners";
+
+import Box from "@/components/ui/box";
+
+const FormPage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [formHTML, setFormHTML] = useState("");
+
+  const { formId } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const formData = await getForm(formId as string);
+        setFormHTML(formData.htmlFormBody);
+      } catch {
+        toast.error("خطایی رخ داده است");
+        navigate("/dashboard");
+      } finally {
+        setIsLoading(false);
+      }
+    })();
+  }, []);
+
+  return (
+    <Box>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div dangerouslySetInnerHTML={{ __html: formHTML }} />
+      )}
+    </Box>
+  );
+};
+export default FormPage;
+
+const Loading = () => {
+  return (
+    <div className="flex size-full items-center justify-center">
+      <MoonLoader color="#2A3042" size={50} />
+    </div>
+  );
+};
