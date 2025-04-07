e505e38ba5fc9e84f818d44cb3cf08ee6d129377
Sepehr Shirazi <sepehrshirazi27@gmail.com>
2025-04-07 07:46:51 +0330

    feat: add workflow items to navbar

--- src/components/layout/navbar/index.tsx
+++ src/components/layout/navbar/index.tsx
@@ -1,6 +1,9 @@
-import { Link } from "react-router-dom";
 import { Home } from "lucide-react";
+import { useEffect, useState } from "react";
 
+import { getWorkflowByRole } from "@/api/workflow";
+import { Link } from "react-router-dom";
+import { MoonLoader } from "react-spinners";
 
 import { cn } from "@/lib/utils";
 
@@ -15,32 +18,64 @@ const Navbar: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({
   className,
   ...props
 }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [items, setItems] = useState<any[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await getWorkflowByRole();
+        setItems(response);
+      } finally {
+        setIsLoading(false);
+      }
+    })();
+  }, []);
+
   return (
-    <div className={cn("h-full w-64 bg-[#2A3042]", className)} {...props}>
+    <div
+      className={cn(
+        "flex h-screen w-64 flex-col overflow-auto bg-[#2A3042]",
+        className
+      )}
+      {...props}
+    >
       <h3 className="py-5 text-center text-xl font-bold text-white">لوگو</h3>
-      <div className="mt-10 text-[#a6b0cf]">
-        <Accordion type="single" collapsible>
-          <AccordionItem value="1">
-            <AccordionTrigger className="px-5 text-sm transition-colors hover:text-white">
-              <div className="flex items-center gap-x-3">
-                <Home size={20} />
-                داشبورد
-              </div>
-            </AccordionTrigger>
-            <AccordionContent className="px-[54px] py-0 text-xs transition-colors hover:text-white">
-              <Link to="/dashboard" className="block py-3">
-                پیش فرض
-              </Link>
-            </AccordionContent>
-            <AccordionContent className="px-[54px] py-0 text-xs transition-colors hover:text-white">
-              <Link to="/dashboard/table" className="block py-3">
-                جدول
-              </Link>
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
+      <div className="mt-10 flex-1 text-[#a6b0cf]">
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <Accordion type="single" collapsible>
+            <AccordionItem value="1">
+              <AccordionTrigger className="px-5 text-sm transition-colors hover:text-white">
+                <div className="flex items-center gap-x-3">
+                  <Home size={20} />
+                  ورک فلو
+                </div>
+              </AccordionTrigger>
+              {items?.map((item) => (
+                <AccordionContent
+                  key={item.id}
+                  className="px-[54px] py-0 text-xs transition-colors hover:text-white"
+                >
+                  <Link to="#" className="block py-3">
+                    {item?.name}
+                  </Link>
+                </AccordionContent>
+              ))}
+            </AccordionItem>
+          </Accordion>
+        )}
       </div>
     </div>
   );
 };
 export default Navbar;
+
+const Loading = () => {
+  return (
+    <div className="flex size-full items-center justify-center">
+      <MoonLoader color="white" size={50} />
+    </div>
+  );
+};
