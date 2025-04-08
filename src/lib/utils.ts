2fd53cd8cf98beea6b7d9033388af3cc9c1ecf21
Sepehr Shirazi <sepehrshirazi27@gmail.com>
2025-04-08 15:48:26 +0330

    feat: implement initial name utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge";
 export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
 };
+
+export const getInitialName = (name: string): string => {
+  const nameParts = name.split(" ").filter(Boolean);
+  const initials = nameParts.slice(0, 2).map((part) => part[0].toUpperCase());
+  return initials.join("");
+};
