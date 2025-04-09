d945d5d1aecb3bbe2618926c2bed48d2a7ec6f7b
Sepehr Shirazi <sepehrshirazi27@gmail.com>
2025-04-09 03:40:07 +0330

    feat: add next item button

--- src/pages/form/index.tsx
+++ src/pages/form/index.tsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
 
 import { getForm } from "@/api/form";
+import { getNextWorkflowValue } from "@/api/workflow";
+import { getSession } from "@/auth";
 import toast from "react-hot-toast";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams, useSearchParams } from "react-router-dom";
 import { MoonLoader } from "react-spinners";
 
 import Box from "@/components/ui/box";
+import { Button } from "@/components/ui/button";
 
 const FormPage = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -13,6 +16,50 @@ const FormPage = () => {
 
   const { formId } = useParams();
   const navigate = useNavigate();
+  const [searchParams, _setSearchParams] = useSearchParams();
+
+  const handleNextFrom = async () => {
+    const session = await getSession();
+
+    const workflowId = searchParams.get("workflowId") as string;
+
+    const loadingToast = toast.loading("درحال دریافت اطلاعات");
+
+    try {
+      const workflowValue = await getNextWorkflowValue(
+        session?.id,
+        +workflowId
+      );
+
+      toast.success("اطلاعات با موفقیت دریافت شد", {
+        id: loadingToast
+      });
+
+      switch (workflowValue.type) {
+        case 2: {
+          navigate(
+            `/dashboard/workflow/form/${workflowValue.dataId}?workflowId=${workflowId}`
+          );
+          break;
+        }
+
+        case 1: {
+          navigate(
+            `/dashboard/workflow/table/${workflowValue.dataId}?workflowId=${workflowId}`
+          );
+          break;
+        }
+
+        default: {
+          break;
+        }
+      }
+    } catch {
+      toast.error("خطا در دریافت اطلاعات", {
+        id: loadingToast
+      });
+    }
+  };
 
   useEffect(() => {
     (async () => {
@@ -48,7 +95,11 @@ const FormPage = () => {
           />
         )}
       </Box>
-      <button>ثبت</button>
+      <div className="mt-10 flex justify-center">
+        <Button onClick={handleNextFrom} className="mx-auto w-32">
+          ثبت
+        </Button>
+      </div>
     </>
   );
 };
