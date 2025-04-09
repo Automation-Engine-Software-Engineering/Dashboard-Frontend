e4bd84a92eefcea1b53ef3ae567209ee5be42aa3
Sepehr Shirazi <sepehrshirazi27@gmail.com>
2025-04-09 21:27:40 +0330

    feat: implement `badge` component

---
+++ src/components/ui/badge/index.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+
+const badgeVariants = cva(
+  "inline-flex items-center justify-center rounded-full border text-xs font-semibold size-5",
+  {
+    variants: {
+      variant: {
+        default: "border-transparent text-white"
+      }
+    },
+    defaultVariants: {
+      variant: "default"
+    }
+  }
+);
+
+export interface BadgeProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof badgeVariants> {}
+
+function Badge({ className, variant, ...props }: BadgeProps) {
+  return (
+    <div className={cn(badgeVariants({ variant }), className)} {...props} />
+  );
+}
+
+export { Badge, badgeVariants };
