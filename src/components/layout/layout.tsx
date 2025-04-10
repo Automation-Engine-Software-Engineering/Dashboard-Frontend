7293d2b25f1576b2c0bd9ffb2710238a7568a991
Sepehr Shirazi <sepehrshirazi27@gmail.com>
2025-04-10 05:45:51 +0330

    feat: add `navbar` to layout

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -2,14 +2,17 @@ import { Outlet } from "react-router-dom";
 
 import Breadcrumb from "../common/breadcrumbs";
 import Header from "./header";
+import Navbar from "./navbar";
 
 const Layout = () => {
   return (
     <>
       <Header />
-      <Breadcrumb />
+      <div className="border-b border-slate-300">
+        <Breadcrumb />
+      </div>
       <div className="relative flex h-full">
-        {/* <Navbar className="shrink-0" /> */}
+        <Navbar className="shrink-0" />
         <div className="flex-1">
           <Outlet />
         </div>
