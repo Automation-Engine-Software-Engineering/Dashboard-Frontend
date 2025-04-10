43c638231cca40775841290910f5ee4cc4939e06
Sepehr Shirazi <sepehrshirazi27@gmail.com>
2025-04-10 15:15:13 +0330

    feat: implement `edit profile` page

---
+++ src/pages/profile/edit-profile/index.tsx
@@ -0,0 +1,10 @@
+import ProfileHeader from "./_components/profile-header";
+
+const EditProfilePage = () => {
+  return (
+    <section>
+      <ProfileHeader />
+    </section>
+  );
+};
+export default EditProfilePage;
