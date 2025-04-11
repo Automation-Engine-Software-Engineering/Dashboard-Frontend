e78f2732d2e5df5974900426b5b7ac0bac9115b9
Sepehr Shirazi <sepehrshirazi27@gmail.com>
2025-04-11 05:29:15 +0330

    feat: implement `save button`

---
+++ src/pages/profile/edit-profile/_components/save-button.tsx
@@ -0,0 +1,15 @@
+const SaveButton = () => {
+  return (
+    <div className="lef-0 fixed bottom-0 flex h-12 w-full items-start bg-white">
+      <div className="flex-1">
+        <div className="h-[20px] border-b border-slate-300 bg-[#F8F8FB]"></div>
+      </div>
+      <div className="flex h-[20px] w-64 justify-center overflow-visible rounded-tr-lg border-r border-t border-slate-300">
+        <button className="mt-3 h-8 rounded-md bg-[#0099A5] px-6 text-sm text-white transition-colors hover:bg-[#0099A5]/90">
+          دخیره تغییرات
+        </button>
+      </div>
+    </div>
+  );
+};
+export default SaveButton;
