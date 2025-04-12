fcae5ee63a492c7d759bd6aead0181f3cff8bb53
Sepehr Shirazi <sepehrshirazi27@gmail.com>
2025-04-12 20:37:51 +0330

    feat: add font `Vazir`

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App.tsx";
-import "./index.css";
 import Providers from "./provider.tsx";
+import "./styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        primary: "hsl(var(--primary))"
+      },
+
+      fontFamily: {
+        vazir: "Vazir"
+      },
+
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
